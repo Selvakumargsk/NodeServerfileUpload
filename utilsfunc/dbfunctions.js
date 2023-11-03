9eb13aee0f8effc4fbaf006ac31f1d4b84f9e2cd
Selvakumargsk <gskumarnellai@gmail.com>
2023-11-03 11:18:37 +0530

    add uploads folder and their subfolder

--- utilsfunc/dbfunctions.js
+++ utilsfunc/dbfunctions.js
@@ -6,32 +6,3 @@ const sequelize = new Sequelize('node_file_handling' , 'root' , 'root' , {
   });
 
 module.exports = sequelize;  
-
-
-// const { Sequelize } = require('sequelize');
-
-// const sequelize = new Sequelize('mysql://root:root@localhost:3306', {
-//   dialect: 'mysql',
-//   logging: false, // You can set this to true for debugging purposes
-//   define: {
-//     timestamps: false, // Disable Sequelize's default timestamps
-//   },
-//   query: {
-//     raw: true, // Use raw SQL for queries
-//   },
-// });
-
-// // Create the database if it doesn't exist
-// async function createDatabaseIfNotExists() {
-//   try {
-//     // This call will create the database if it doesn't exist
-//     await sequelize.query(`CREATE DATABASE IF NOT EXISTS node_file_handling;`);
-//     console.log('Database created or already exists.');
-//   } catch (error) {
-//     console.error('Error creating database:', error);
-//   }
-// }
-
-// createDatabaseIfNotExists(); // Call the function to create the database
-
-// module.exports = sequelize;
